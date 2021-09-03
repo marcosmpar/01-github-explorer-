8c967d40140d1b5883ab850aab1d3299036215f9
Marcos Rodrigues <marcosmpar@gmail.com>
2021-09-03 11:08:53 -0300

    primeiro component react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,33 @@
+export function RepositoryList(){
+    return (
+        <section className="respository-list">
+            <h1>Lista de respositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
